ab70fedac358b6fc3ebf369a8b0ba1c5f27b5337
Evan Ruby <evanruby94@gmail.com>
2024-01-16 18:22:00 -0500

    update hero image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,16 +6,16 @@ const Homepage = () => {
   return (
     <div className="page-root pt-[80px] pb-12 flex items-center justify-center flex-col">
       <div
-        className="min-h-[500px] w-full bg-[center_bottom] bg-no-repeat bg-cover relative mb-12"
-        style={{ backgroundImage: `url(/images/movement.jpg)` }}
+        className="min-h-[500px] w-full bg-center bg-no-repeat bg-cover relative mb-12"
+        style={{ backgroundImage: `url(/images/hero.jpg)` }}
       >
         <div className="bg-black absolute top-0 left-0 right-0 bottom-0 opacity-40">
           <Image
             className="w-[1px] h-auto opacity-0"
-            src="/images/movement.jpg"
+            src="/images/hero.jpg"
             width={2016}
             height={1135}
-            alt="Evan standing in front of a sign at Movement in 2023"
+            alt="Evan standing in front of a street-art backdrop"
           />
         </div>
       </div>
